be752ca80a5a4fedea13add4a576fc36c2e0110f
Henry Ndou <ndou.research@gmail.com>
2023-06-25 16:21:46 +0200

    Whatever was worng with the main.dart file sisharp

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:timesheet_manager_app/responsive_screen/mobile_screen_layout.dart';
 import 'package:timesheet_manager_app/responsive_screen/web_screen_layout.dart';
-import 'package:timesheet_manager_app/screens/homepage.dart';
 import 'package:timesheet_manager_app/screens/login_screen.dart';
 import 'package:timesheet_manager_app/screens/signup_screen.dart';
 import 'package:timesheet_manager_app/utilities/colours.dart';
 import 'package:timesheet_manager_app/widgets/text_field_input.dart';
 import 'responsive_screen/responsive_layout.dart';
-import 'screens/widgets/sidebar_menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.light().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      home: HomePage(),
+      home: MobileScreenLayout(),
     );
   }
 }
