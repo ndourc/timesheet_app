88b67cc83664815b3b56308fe48d2f9e9fc942eb
Henry Ndou <ndou.research@gmail.com>
2023-06-26 13:51:00 +0200

    Flutter upgrade

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,7 +7,7 @@ var myAppBar = AppBar(
 
 var myDrawer = Drawer(
           child: Column(
-            children: const [
+            children: [
             DrawerHeader(
               child: Icon(Icons.groups),
             ),
@@ -16,36 +16,42 @@ var myDrawer = Drawer(
                   Icons.admin_panel_settings,
                 ),
                 title: Text('Admin Dashboard'),
+                onTap: () {},
             ),
             ListTile(
               leading: Icon(
                   Icons.stacked_line_chart,
                 ),
                 title: Text('Client Statistics'),
+                onTap: () {},
               ),
             ListTile(
               leading: Icon(
                   Icons.settings,
                 ),
                 title: Text('Settings'),
+                onTap: () {},
               ),
               ListTile(
                 leading: Icon(
                     Icons.calendar_month,
                   ),
-                  title: Text('Schedule'),
+                  title: Text('Calendar'),
+                  onTap: () {},
                 ),
               ListTile(
                 leading: Icon(
                     Icons.toggle_off_outlined,
                   ),
                   title: Text('Change Theme'),
+                  onTap: () {},
                 ),
                 ListTile(
                 leading: Icon(
                     Icons.logout_rounded,
                   ),
                   title: Text('Logout'),
+                  onTap: () {},
               ),
             ],
           ),
